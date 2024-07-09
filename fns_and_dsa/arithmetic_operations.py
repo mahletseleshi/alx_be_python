aeb3763a42bdc82ef8ba2be955cf5f03115481aa
Mahlet Seleshi <74148907+mahletseleshi@users.noreply.github.com>
2024-07-09 09:57:34 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
-    if operation == "substract":
+    if operation == "subtract":
         return num1 - num2
     if operation == "multiply":
         return num1 * num2
