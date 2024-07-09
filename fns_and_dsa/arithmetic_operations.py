2d39cbd7dbe25a3015c58a985bbe04d5d5b97a75
Mahlet Seleshi <74148907+mahletseleshi@users.noreply.github.com>
2024-07-09 09:58:47 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,11 +5,10 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     if operation == "multiply":
         return num1 * num2
-    if operation == "divide":
-        if num2 == '0':
-            return "Error: Invalid division"
-        else:
-            return num1 / num2
+    if operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
     else:
         return "Invalid Operation"
 
