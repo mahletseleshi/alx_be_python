b2c065d363721035f4ab940ff9084179807e330e
Mahlet Seleshi <mahletseleshi224@gmail.com>
2024-07-14 12:45:50 +0300

    intial commit

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,56 @@
+class Book:
+    """A class representing a book in the library."""
+    
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as available."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    """A class representing a library."""
+    
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if it's available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                return book.check_out()
+        return False
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                return book.return_book()
+        return False
+
+    def list_available_books(self):
+        """List all available books in the library."""
+        available_books = [book for book in self._books if book.is_available()]
+        for book in available_books:
+            print(f"{book.title} by {book.author}")
