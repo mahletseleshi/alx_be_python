3965cb14dc68e98498652e7a1550fd94d6e861ce
Mahlet Seleshi <mahletseleshi224@gmail.com>
2024-07-21 11:17:48 +0300

    intial commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,12 +1,13 @@
 # library_system.py
 
+
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
@@ -14,7 +15,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} [EBook, {self.file_size}KB]"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -22,7 +23,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} [Print, {self.page_count} pages]"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
