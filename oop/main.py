f162041ffd9f1ddd133ecfc2019275d8a0408520
Mahlet Seleshi <mahletseleshi224@gmail.com>
2024-07-21 11:03:39 +0300

    intial commit

--- oop/main.py
+++ oop/main.py
@@ -1,14 +1,17 @@
-class Book:
-    def __init__(self, title, author, year):
-        self.title = title
-        self.author = author
-        self.year = year
+from book_class import Book
 
-    def __del__(self):
-        print(f"Deleting {self.title}")
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
 
-    def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
 
-    def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
